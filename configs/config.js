44b431ace81cbc1b83bd0778ae14b94bc098b688
YarneS21 <yarne@mail.be>
2023-05-01 14:59:37 +0200

    qdf

--- configs/config.js
+++ configs/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     developersIds: ["184333852267905024", "923541283383951430"],
     embedColor: "#75E6DA",
-    logChannel: "1070789852754366504",
+    logChannel: "1099031745833214033", // TEST = 1070789852754366504 // LIVE = 1099031745833214033    
     picture: "https://cdn.discordapp.com/attachments/989540592725950515/1102291494481690744/icon-1.png",
     footer: "YourForums Bot",
     tick: "<:tick:1102277732865429584>",
