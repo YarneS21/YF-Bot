a1a47dcdb98a7bd890e91406edf68b97e2f33bd3
YarneS21 <yarne@mail.be>
2023-05-01 17:34:19 +0200

    d

--- configs/config.js
+++ configs/config.js
@@ -4,8 +4,8 @@ module.exports = {
     logChannel: "1099031745833214033", // TEST = 1070789852754366504 // LIVE = 1099031745833214033    
     picture: "https://cdn.discordapp.com/attachments/989540592725950515/1102291494481690744/icon-1.png",
     footer: "YourForums Bot",
-    tick: "<:tick:1102277732865429584>",
-    reply: "<:reply:1102277690167410819>",
-    replyc: "<:replycont:1102277687076212826>",
+    tick: "<:tick:1040008503617671261>",
+    reply: "<:reply:1102276890946973867>",
+    replyc: "<:replycont:1102276889843871874>",
     welcomeChannel: "1051155398264098836"
 }
