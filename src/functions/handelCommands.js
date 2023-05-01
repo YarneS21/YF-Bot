cb5d0943324446cba44b7b3a179615bd19420596
YarneS21 <yarne@mail.be>
2023-05-01 14:53:20 +0200

    fix

--- src/functions/handelCommands.js
+++ src/functions/handelCommands.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 
 const clientId = '1101866908833300562'; 
 const guildId = '1043931375817994370'; 
-const credentials = require('../../configs/')
 
 module.exports = (client) => {
     client.handleCommands = async (commandFolders, path) => {
