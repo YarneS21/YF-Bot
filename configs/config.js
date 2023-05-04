8f6c181d19432b34d64754d89f0d38599135b60c
YarneS21 <yarne@mail.be>
2023-05-04 19:04:28 +0200

    d

--- configs/config.js
+++ configs/config.js
@@ -1,11 +1,11 @@
 module.exports = {
     developersIds: ["184333852267905024", "923541283383951430"],
     embedColor: "#75E6DA",
-    logChannel: "1099031745833214033", // TEST = 1070789852754366504 // LIVE = 1099031745833214033    
+    logChannel: "1103726381164150864", // TEST = 1070789852754366504 // LIVE = 1099031745833214033    
     picture: "https://cdn.discordapp.com/attachments/989540592725950515/1102291494481690744/icon-1.png",
     footer: "YourForums Bot",
     tick: "<:tick:1040008503617671261>",
     reply: "<:reply:1102276890946973867>",
     replyc: "<:replycont:1102276889843871874>",
-    welcomeChannel: "1099031745833214033"
+    welcomeChannel: "1103726381164150864"
 }
