0b3cb98bc6fa0bee5c607ff4dce1ddb03a0df385
YarneS21 <yarne@mail.be>
2023-05-07 12:04:36 +0200

    d

--- src/index.js
+++ src/index.js
@@ -117,15 +117,13 @@ client.on(Events.MessageCreate, async message => {
     if (message.channel.id == config.suggestionChannel) {
         const string = message.content
         const author = message.author.id
+        message.delete()
         let embed = EmbedBuilder()
         .setAuthor(author)
-        .setTitle(`New Suggestion`)
         .setDescription(string)
-        .setThumbnail(message.author.displayAvatarURL({dynamic: true}))
-        .setTimestamp()
-        let newMessage = config.suggestionChannel.send({embeds: [embed]})
-        newMessage.react("<:tick:1040008503617671261>")
-        newMessage.react("<:cross:1040008993457836174>")
+        .setThumbnail(message.author.displayAvatarURL())
+        message.react("<:tick:1040008503617671261>")
+        message.react("<:cross:1040008993457836174>")
     }
 })
 
